e5a6555230587ad53fddcaaa7a51758aa23dca8b
BAN Sothen <ban.sothen2016@rupp.edu.kh>
2024-01-23 09:16:36 +0700

    remove external models

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ if audio_out[-4:] != ".mp3":
 
 audio_out = os.path.normpath(audio_out)
 
-script_dir = os.path.dirname(os.path.abspath(__file__)) + "/models/"
-model_dir = os.path.join(script_dir, "base.en.pt")
+# script_dir = os.path.dirname(os.path.abspath(__file__)) + "/models/"
+# model_dir = os.path.join(script_dir, "base.en.pt")
 
-result = model.transcribe(audio_out, verbose=False, fp16=False, download_root=model_dir)
+# result = model.transcribe(audio_out, verbose=False, fp16=False, download_root=model_dir)
+result = model.transcribe(audio_out, verbose=False, fp16=False)
 transcription_data = result["transcription"]
 
 # Specify the path and name of the Word file
